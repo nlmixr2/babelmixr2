69d151adcfd8116d647bbf97fa2e43a342f23de5
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-04-27 16:45:45 -0500

    Start exporting the ui properties better

--- R/monolixRxUiGet.R
+++ R/monolixRxUiGet.R
@@ -5,6 +5,7 @@ rxUiGet.getMonolixResidualDistribution <- function(x, ...) {
   #   .varp <- str2lang(paste0("rx_pred_", .pred1[["var"]]))
   paste0("rx_prd_", .predDf$var, "={distribution = normal, prediction = rx_pred_", .predDf$var, ", errorModel=")
 }
+
 #' @export
 rxUiGet.getMonolixModel0 <- function(x, ...) {
   .ui <- x[[1]]
@@ -20,7 +21,6 @@ rxUiGet.getMonolixModel0 <- function(x, ...) {
                     lstExpr=.split$modelWithDrop,
                     useIf=FALSE)
   .norm <- rxode2::rxNorm(eval(.mainModel))
-  .def <- paste(paste0(names(.resMod), "_pred= {distribution = normal, prediction = ", names(.resMod), ", errorModel=",
-                       monolixGetErr(.resMod, uif, control), "}"), collapse="\n")
   .mod <- rxToMonolix(.norm)
+  .mod
 }
--- R/reexport.R
+++ R/reexport.R
@@ -1,3 +1,9 @@
 #' @importFrom rxode2 rxModelVars
 #' @export
 rxode2::rxModelVars
+
+
+#' @importFrom rxode2 rxUiGet
+#' @export
+rxode2::rxUiGet
+
