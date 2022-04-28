1809b6cc34d5035f7c6ea652291741068ef3daf9
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-04-28 08:05:02 -0500

    Start residual drift

--- R/monolixRxUiGet.R
+++ R/monolixRxUiGet.R
@@ -1,9 +1,44 @@
+.getMonolixResidual <- function(ui, i) {
+  .pred1 <- ui$predDf[i, ]
+  .iniDf <- ui$iniDf
+  .cond <- .pred1$cond
+  .errType <- .pred1$errType
+  .iniDf <- .iniDf[which(.iniDf$condition == .cond), ]
+  if (.errType == 1L) { # add
+
+  } else if (.errType == 2L) { # prop
+
+  } else if (.errType == 3L) { # pow
+
+  } else if (.errType == 4L) { # add + prop
+    .addProp <- .predDf$addProp
+    if (.addProp == "default") {
+      .addProp <- rxode2::rxGetControl(ui, "addProp", "combined2")
+    }
+    .add <- .iniDf[.iniDf$err == "add", "name"]
+    .add <- eval(str2lang(paste0("rxToMonolix(", .add, ")")))
+    .prop <- .iniDf[.iniDf$err == "prop", "name"]
+    .prop <- eval(parse(text=paste0("rxToMonolix(", .prop, ")")))
+    .lst[[length(.lst) + 1]] <- data.frame(name=.ini[.ini$err == "prop", "name"], errName=.prop)
+    assignInMyNamespace(".monolixErrs", c(.monolixErrs, .add, .prop))
+    assignInMyNamespace(".monolixGetErr", .lst)
+    return(paste0(control$addProp, "(", .add, ",", .prop, ")"))
+
+  } else if (.errType == 5L) { #  add + pow
+
+  } else {
+    stop("residual type 'none' is not supported in monolix",
+         call.=FALSE)
+  }
+}
+
 #' @export
 rxUiGet.getMonolixResidualDistribution <- function(x, ...) {
   .ui <- x[[1]]
   .predDf <- .ui$predDf
   #   .varp <- str2lang(paste0("rx_pred_", .pred1[["var"]]))
-  paste0("rx_prd_", .predDf$var, "={distribution = normal, prediction = rx_pred_", .predDf$var, ", errorModel=")
+  .iniDf <- .ui$iniDf
+  paste0("rx_prd_", .predDf$var, "={distribution = normal, prediction = rx_pred_", .predDf$var, ", errorModel=", "}")
 }
 
 #' @export
