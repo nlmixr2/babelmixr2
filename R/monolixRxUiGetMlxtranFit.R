61a87b65488778b6c5b8f433e122d949bfce1400
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-05-16 13:38:06 -0500

    Match data and model names with single endpoint models

--- R/monolixRxUiGetMlxtranFit.R
+++ R/monolixRxUiGetMlxtranFit.R
@@ -2,7 +2,11 @@
 rxUiGet.mlxtranFit <- function(x, ...) {
   .ui <- x[[1]]
   .predDf <- .ui$predDf
-  .data <- paste0("data={", paste(paste0("y", .predDf$dvid), collapse=", "), "}")
+  if (length(.predDf$cond) == 1L) {
+    .data <- paste0("data={", paste(paste0("rx_prd_", .predDf$var), collapse=", "), "}")
+  } else {
+    .data <- paste0("data={", paste(paste0("y", .predDf$dvid), collapse=", "), "}")
+  }
   .model <- paste0("model={", paste(paste0("rx_prd_", .predDf$var), collapse=", "), "}")
   paste(c("<FIT>", .data, .model), collapse="\n")
 }
