5aa7c10f7717347afa75e2a147e31c2bc5e52006
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-05-17 13:52:31 -0500

    Only check call[[2]] pars that are len=1

--- R/splitModelRxUiGet.R
+++ R/splitModelRxUiGet.R
@@ -25,22 +25,24 @@
       .call <- expr[[3]]
       .char <- as.character(expr[[2]])
       .callName <- as.character(.call[[1]])
-      .w <- which(muRefCurEval$parameter == as.character(.call[[2]]))
-      if (length(.w) == 1L) {
-        if (muRefCurEval$curEval[.w] == .callName) {
-          # This is an additive mu reference expression
-          # c(tcl="cl")
-          .low <- muRefCurEval$low[.w]
-          .hi <- muRefCurEval$hi[.w]
-          if (length(.call) == 2) {
-            return(setNames(.char, as.character(.call[[2]])))
-          } else if (length(.call) == 3 && .call[[3]] == ifelse(is.na(.low), 0, .low)) {
-            return(setNames(.char, as.character(.call[[2]])))
-          } else if (length(.call) == 4 &&
-                       .call[[3]] == ifelse(is.na(.low), 0, .low) &&
-                       .call[[4]] == ifelse(is.na(.hi), 1, .hi) &&
-                       TRUE) {
-            return(setNames(.char, as.character(.call[[2]])))
+      if (length(.call[[2]]) == 1) {
+        .w <- which(muRefCurEval$parameter == as.character(.call[[2]]))
+        if (length(.w) == 1L) {
+          if (muRefCurEval$curEval[.w] == .callName) {
+            # This is an additive mu reference expression
+            # c(tcl="cl")
+            .low <- muRefCurEval$low[.w]
+            .hi <- muRefCurEval$hi[.w]
+            if (length(.call) == 2) {
+              return(setNames(.char, as.character(.call[[2]])))
+            } else if (length(.call) == 3 && .call[[3]] == ifelse(is.na(.low), 0, .low)) {
+              return(setNames(.char, as.character(.call[[2]])))
+            } else if (length(.call) == 4 &&
+                         .call[[3]] == ifelse(is.na(.low), 0, .low) &&
+                         .call[[4]] == ifelse(is.na(.hi), 1, .hi) &&
+                         TRUE) {
+              return(setNames(.char, as.character(.call[[2]])))
+            }
           }
         }
       }
