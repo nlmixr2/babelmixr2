4be13eab5f2796781a99416190c4f37e1f75e8dd
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-05-24 15:13:16 -0500

    Add some dtbs for each group
    Signed-off-by: Matthew Fidler <514778+mattfidler@users.noreply.github.com>

--- R/nonmemRxUiGetErr.R
+++ R/nonmemRxUiGetErr.R
@@ -1,10 +1,44 @@
+.getErr <- function(err) {
+  .err <- readLines(file.path(system.file(package="babelmixr2"), err))
+  paste0("$ERROR\n",
+         paste(gsub("^      ", "  ", .err), collapse="\n"))
+}
+
 #'@export
 rxUiGet.nonmemErr <- function(x, ...) {
-  paste(c("$ERROR\n",
-           "  IPRED = RX_PRED\n",
-           "  IRES = DV = IPRED\n",
-           "  W     = DSQRT(W)\n",
-           "  IF (W .EQ. 0)  W = 1\n",
-           "  IWRES = IRES/W\n",
-           "  Y = IPRED + EPS(1)*W\n"), collapse="")
+  .ui <- x[[1]]
+  .predDf <- .ui$predDf
+  if (length(.predDf$cond) == 1L) {
+    .iniDf <- .ui$iniDf
+    if (.predDf$transform == "untransformed") return(.getErr("err.txt"))
+    .transform <- paste(.predDf$transform)
+    .low <- paste(.predDf$trLow)
+    if (regexpr("^[0-9]+$", .low) != -1) .low <- paste0(.low, ".0")
+    .hi <- paste(.predDf$trHi)
+    if (regexpr("^[0-9]+$", .hi) != -1) .hi <- paste0(.hi, ".0")
+    if (.transform == "boxCox") {
+      .w <- which(.iniDf$err == "boxCox")
+      .t <- (.nonmemGetThetaNum(.iniDf$name[.w],.ui))
+      return(gsub("LAMBDA", .t, .getErr("err-boxCox.txt")))
+    } else if (.transform == "yeoJohnson") {
+      # The rest are not
+      .w <- which(.iniDf$err == "yeoJohnson")
+      .t <- (.nonmemGetThetaNum(.iniDf$name[.w],.ui))
+      return(gsub("LAMBDA", .t, .getErr("err-yeoJohnson.txt")))
+    } else if (.transform == "lnorm") {
+      return(.getErr("err-lnorm.txt"))
+    } else if (.transform == "logit") {
+      return(gsub("LOW", .low,
+                  gsub("HIGH", .hi,
+                       .getErr("err-logit.txt"))))
+    } else if (.transform == "logit + yeoJohnson") {
+      .w <- which(.iniDf$err == "yeoJohnson")
+      .t <- (.nonmemGetThetaNum(.iniDf$name[.w],.ui))
+      return(gsub("LAMBDA", .t,
+                  gsub("LOW", .low,
+                       gsub("HIGH", .hi,
+                            .getErr("err-logitYeoJohnson.txt")))))
+    }
+  }
+  return(.getErr("err.txt"))
 }
--- tests/testthat/test-nonmem.R
+++ tests/testthat/test-nonmem.R
@@ -158,10 +158,13 @@ test_that("tbs tests", {
   p <- pheno()
 
   expect_equal(p$nonmemCcontra,
-               "      subroutine ccontr (icall,c1,c2,c3,ier1,ier2)\n      USE ROCM_REAL,   ONLY: theta=>THETAC,y=>DV_ITM2\n      USE NM_INTERFACE,ONLY: CELS\n!      parameter (lth=40,lvr=30,no=50)\n!      common /rocm0/ theta (lth)\n!      common /rocm4/ y\n!      double precision c1,c2,c3,theta,y,w,one,two\n      double precision c1,c2,c3,w,one,two\n      dimension c2(:),c3(:,:)\n      data one,two/1.,2./\n      if (icall.le.1) return\n      w=y(1)\n\n         if(theta(4).eq.0) y(1)=log(y(1))\n         if(theta(4).ne.0) y(1)=(y(1)**theta(4)-one)/theta(4)\n\n\n      call cels (c1,c2,c3,ier1,ier2)\n      y(1)=w\n      c1=c1-two*(theta(4)-one)*log(y(1))\n\n      return\n      end")
+               "      subroutine ccontr (icall,c1,c2,c3,ier1,ier2)\n      USE ROCM_REAL,   ONLY: theta=>THETAC,y=>DV_ITM2\n      USE NM_INTERFACE,ONLY: CELS\n!      parameter (lth=40,lvr=30,no=50)\n!      common /rocm0/ theta (lth)\n!      common /rocm4/ y\n!      double precision c1,c2,c3,theta,y,w,one,two\n      double precision c1,c2,c3,w,one,two\n      dimension c2(:),c3(:,:)\n      data one,two/1.,2./\n      if (icall.le.1) return\n      w=y(1)\n      if(theta(4).eq.0) y(1)=log(y(1))\n      if(theta(4).ne.0) y(1)=(y(1)**theta(4)-one)/theta(4)\n      call cels (c1,c2,c3,ier1,ier2)\n      y(1)=w\n      c1=c1-two*(theta(4)-one)*log(y(1))\n      return\n      end")
+
+  expect_equal(p$nonmemErr,
+               "$ERROR\n  IPRED = RX_PRED\n  W     = DSQRT(W)\n  IF (W .EQ. 0.0) W = 1.0\n  IF (THETA(4) .EQ. 0.0 .AND. IPRED .NE. 0.0) THEN\n     IPRED = DLOG(IPRED)\n  ELSE IF (THETA(4) .EQ. 0.0 .AND. IPRED .EQ. 0.0) THEN\n     IPRED = -1/THETA(4)\n  ELSE IF (THETA(4) .NE. 0.0 .AND. IPRED .NE. 0.0) THEN\n     IPRED = (IPRED**THETA(4) - 1.0)/THETA(4)\n  ELSE IF (THETA(4) .NE. 0.0 .AND. IPRED .EQ. 0.0) THEN\n     IPRED = -1000000000\n  END IF\n  Y     = IPRED + EPS(1)*W")
 
 
-  pheno <- function() {
+   pheno <- function() {
     ini({
       tcl <- log(0.008) # typical value of clearance
       tv <-  log(0.6)   # typical value of volume
@@ -185,6 +188,9 @@ test_that("tbs tests", {
   p <- pheno()
 
   expect_equal(p$nonmemCcontra, "      subroutine ccontr (icall,c1,c2,c3,ier1,ier2)\n      USE ROCM_REAL,   ONLY: theta=>THETAC,y=>DV_ITM2\n      USE NM_INTERFACE,ONLY: CELS\n!      parameter (lth=40,lvr=30,no=50)\n!      common /rocm0/ theta (lth)\n!      common /rocm4/ y\n!      double precision c1,c2,c3,theta,y,w,one,two\n      double precision c1,c2,c3,w,one,two\n      dimension c2(:),c3(:,:)\n      data one,two/1.,2./\n      if (icall.le.1) return\n      w=y(1)\n      if (theta(4) .eq. 1.0) then\n         y(1) = y(1)\n      else if (y(1) .gt. 0.0) then\n         if (theta(4) .eq. 0.0) then\n            y(1) = log(y(1) + one)\n         else\n            y(1) = ((y(1)+one)**theta(4)-one)/theta(4)\n         end if\n      else\n         if (theta(4) .eq. 2.0) then\n            y(1) = -log(one - y(1))\n         else\n            y(1) = (1.0 - (1.0- y(1))**(2.0-theta(4)))/(2.0 - theta(4))\n         end if\n      end if\n      call cels (c1,c2,c3,ier1,ier2)\n      y(1)=w\n      if (y(1) .ge. 0) then\n         c1=c1-two*(theta(4)-one)*log(one+y(1))\n      else\n         c1=c1-two*(one-theta(4))*log(one-y(1))\n      end if\n      return\n      end")
+
+  expect_equal(p$nonmemErr,
+               "$ERROR\n  IPRED = RX_PRED\n  W     = DSQRT(W)\n  IF (W .EQ. 0.0) W = 1.0\n  IF (IPRED .GE. 0.0) THEN\n     IF (THETA(4) .EQ. 0.0) THEN\n        IPRED = DLOG(IPRED + 1.0)\n     ELSE IF (THETA(4) .EQ. 1) THEN\n        IPRED = IPRED\n     ELSE\n        IPRED = ((IPRED+1.0)**THETA(4) - 1.0)/THETA(4)\n     END IF \n  ELSE\n     IF (THETA(4) .EQ. 2.0) THEN\n        IPRED = -DLOG(1.0 - IPRED)\n     ELSE IF  (THETA(4) .EQ. 1.0) THEN\n        IPRED = IPRED\n     ELSE\n        IPRED = (1.0 - (1.0 - IPRED)**(2.0 - THETA(4)))/(2.0 - THETA(4))\n     END IF\n  END IF\n  Y = IPRED + EPS(1)*W")
 
   pheno <- function() {
     ini({
@@ -211,6 +217,10 @@ test_that("tbs tests", {
   expect_equal(p$nonmemCcontra,
                "      subroutine ccontr (icall,c1,c2,c3,ier1,ier2)\n      USE ROCM_REAL,   ONLY: theta=>THETAC,y=>DV_ITM2\n      USE NM_INTERFACE,ONLY: CELS\n!      parameter (lth=40,lvr=30,no=50)\n!      common /rocm0/ theta (lth)\n!      common /rocm4/ y\n!      double precision c1,c2,c3,theta,y,w,one,two\n      double precision c1,c2,c3,w,one,two\n      dimension c2(:),c3(:,:)\n      data one,two/1.,2./\n      if (icall.le.1) return\n      w=y(1)\n      y(1)=log(y(1))\n      call cels (c1,c2,c3,ier1,ier2)\n      y(1)=w\n      c1=c1+two*log(y(1))\n      return\n      end")
 
+  expect_equal(p$nonmemErr,
+               "$ERROR\n  IPRED = RX_PRED\n  W     = DSQRT(W)\n  IF (W .EQ. 0.0) W = 1.0\n  IF (IPRED .GE. 0.0) THEN\n     IF (THETA(4) .EQ. 0.0) THEN\n        IPRED = DLOG(IPRED + 1.0)\n     ELSE IF (THETA(4) .EQ. 1) THEN\n        IPRED = IPRED\n     ELSE\n        IPRED = ((IPRED+1.0)**THETA(4) - 1.0)/THETA(4)\n     END IF \n  ELSE\n     IF (THETA(4) .EQ. 2.0) THEN\n        IPRED = -DLOG(1.0 - IPRED)\n     ELSE IF  (THETA(4) .EQ. 1.0) THEN\n        IPRED = IPRED\n     ELSE\n        IPRED = (1.0 - (1.0 - IPRED)**(2.0 - THETA(4)))/(2.0 - THETA(4))\n     END IF\n  END IF\n  Y = IPRED + EPS(1)*W")
+
+
   pheno <- function() {
     ini({
       tcl <- log(0.008) # typical value of clearance
@@ -236,6 +246,8 @@ test_that("tbs tests", {
   expect_equal(p$nonmemCcontra,
                "      subroutine ccontr (icall,c1,c2,c3,ier1,ier2)\n      USE ROCM_REAL,   ONLY: theta=>THETAC,y=>DV_ITM2\n      USE NM_INTERFACE,ONLY: CELS\n!      parameter (lth=40,lvr=30,no=50)\n!      common /rocm0/ theta (lth)\n!      common /rocm4/ y\n!      double precision c1,c2,c3,theta,y,w,one,two\n      double precision c1,c2,c3,w,one,two,xl,hl,hl2\n      dimension c2(:),c3(:,:)\n      data one,two/1.,2./\n      if (icall.le.1) return\n      w=y(1)\n      xl = (y(1)-(-0.1))/((70.0)-(-0.1))\n      y(1) = -log(one/xl-one)\n      call cels (c1,c2,c3,ier1,ier2)\n      y(1)=w\n      xl = (y(1)-(-0.1))\n      hl = ((70.0) - (-0.1))\n      hl2 = hl-xl\n      c1=c1-two*(log(hl)-log(xl)-log(hl2))\n      return\n      end")
 
+  expect_error(p$nonmemError,
+  "$ERROR\n  IPRED = RX_PRED\n  W     = DSQRT(W)\n  XL  = (IPRED - (-0.1))/((70.0) - (-0.1))\n  IPRED = -DLOG(1.0/XL - 1.0)\n  Y     = IPRED + EPS(1)*W")
 
   pheno <- function() {
     ini({
@@ -260,7 +272,11 @@ test_that("tbs tests", {
 
   p <- pheno()
 
-  message(p$nonmemCcontra)
+  expect_equal(p$nonmemErr,
+               "$ERROR\n  IPRED = RX_PRED\n  W     = DSQRT(W)\n  XL  = (IPRED - (-0.1))/((70.0) - (-0.1))\n  XL  = -DLOG(1.0/XL - 1.0)\n  IF (THETA(4) .EQ. 1.0) THEN\n     IPRED = XL\n  ELSE IF (XL .GE. 0.0) THEN\n     IF (THETA(4) .EQ. 0.0) THEN\n        IPRED = DLOG(1.0 + XL)\n     ELSE\n        IPRED = ((XL + 1.0)**THETA(4) - 1.0)/THETA(4)\n     END IF\n  ELSE\n     IF (THETA(4) .EQ. 2.0) THEN\n        IPRED = -LOG(1.0 - XL)\n     ELSE\n        HL = 2.0 - THETA(4)\n        IPRED = (1.0 (1.0 - XL)**HL)/HL\n     END IF\n  END IF\n  Y     = IPRED + EPS(1)*W")
+
+  expect_equal(p$nonmemCcontra,
+               "      subroutine ccontr (icall,c1,c2,c3,ier1,ier2)\n      USE ROCM_REAL,   ONLY: theta=>THETAC,y=>DV_ITM2\n      USE NM_INTERFACE,ONLY: CELS\n!      parameter (lth=40,lvr=30,no=50)\n!      common /rocm0/ theta (lth)\n!      common /rocm4/ y\n!      double precision c1,c2,c3,theta,y,w,one,two\n      double precision c1,c2,c3,w,one,two,xl,hl,hl2,pd,pdd1,pdd2,p\n      dimension c2(:),c3(:,:)\n      data one,two/1.,2./\n      if (icall.le.1) return\n      w=y(1)\n      xl = (y(1)-(-0.1))/((70.0)-(-0.1))\n      xl = -log(one/xl-one)\n      if (theta(4) .eq. 1.0) then\n         y(1) = xl\n      else if (xl .ge. 0) then\n         if (theta(4) .eq. 0) then\n            y(1) = log(one+xl)\n         else\n            y(1) = ((xl + one)**theta(4) - one)/theta(4)\n         end if\n      else\n         if (theta(4) .eq. 2.0) then\n            y(1) = -log(one-xl)\n         else\n            hl = two - theta(4)\n            y(1) = (one - (one - xl)**hl)/hl\n         end if\n      end if\n      call cels (c1,c2,c3,ier1,ier2)\n      y(1)=w\n      p = (y(1)-(-0.1))/((70.0)-(-0.1))\n      pd = -log(one/p-one)\n      if (theta(4) .eq. one) then\n         pdd1 = 1.0\n      else if (pd .ge. 0) then\n         if (theta(4) .eq. 0.0) then\n            pdd1 = one/(pd + one)\n         else\n            pdd1 = (pd + one)**(theta(4)-1.0)\n         end if\n      else \n         if (theta(4) .eq. 2.0) then\n            pdd1 =  -one/(one - pd);\n         else\n            pdd1 = (one - pd)**(one-theta(4))\n         end if\n      end if\n      xl = (y(1)-(-0.1))\n      hl = ((70.0) - (-0.1));\n      pdd2 = hl/(xl*(hl-xl));\n      c1=c1-two*(log(pdd1)+log(pdd2))\n      return\n      end")
 
 })
 
