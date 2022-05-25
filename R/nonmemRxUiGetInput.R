ddf1e3867fe164c81274e68ef245c5d292480f5a
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-05-25 16:04:03 -0500

    Be more careful with the $INPUT

--- R/nonmemRxUiGetInput.R
+++ R/nonmemRxUiGetInput.R
@@ -1,8 +1,16 @@
 #' @export
 rxUiGet.nonmemInput <- function(x, ...) {
   .ui <- x[[1]]
-  paste(paste(c("$INPUT ID TIME EVID AMT II DV CMT SS",
-         vapply(.ui$allCovs, .nmGetVar, character(1), ui=.ui,
-                USE.NAMES=FALSE), "ROW_"),
-        collapse=" "), "\n")
+  .ret <- c("$INPUT ID TIME EVID AMT",
+                ifelse(rxode2::rxGetControl(.ui, ".hasIi", FALSE), "II", ""),
+                "DV CMT",
+                ifelse(rxode2::rxGetControl(.ui, ".hasSs", FALSE), "SS", ""),
+                ifelse(rxode2::rxGetControl(.ui, ".hasRate", FALSE), "RATE", ""),
+                vapply(.ui$allCovs, .nmGetVar, character(1), ui=.ui,
+                       USE.NAMES=FALSE),
+                ifelse(rxode2::rxGetControl(.ui, ".hasCens", FALSE), "CENS", ""),
+                ifelse(rxode2::rxGetControl(.ui, ".hasLimit", FALSE), "LIMIT", ""),
+            "DROP")
+  .ret <- .ret[.ret != ""]
+  paste0(paste(.ret, collapse=" "), "\n")
 }
