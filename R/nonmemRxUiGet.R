69929362ff9364d90bf963be233c82fd45c98078
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-06-10 12:00:16 -0500

    Start matching naming conventions

--- R/nonmemRxUiGet.R
+++ R/nonmemRxUiGet.R
@@ -1,47 +1,52 @@
 #' @export
 rxUiGet.nonmemExportPath <- function(x, ...) {
   .ui <- x[[1]]
-  .ui$modelName
+  .extra <- ""
+  .num <- rxode2::rxGetControl(.ui, ".modelNumber", 0)
+  if (.num > 0) {
+    .extra <- sprintf("-%03d", .num)
+  }
+  paste0(.ui$modelName, .extra, "-nonmem")
 }
 
 #' @export
 rxUiGet.nonmemEtaTableName <- function(x, ...) {
   .ui <- x[[1]]
-  paste0(.ui$modelName, ".eta")
+  paste0(rxUiGet.nonmemExportPath(x, ...), ".eta")
 }
 
 #' @export
 rxUiGet.nonmemSdTableName <- function(x, ...) {
   .ui <- x[[1]]
-  paste0(.ui$modelName, ".pred")
+  paste0(rxUiGet.nonmemExportPath(x, ...), ".pred")
 }
 
 #' @export
 rxUiGet.nonmemContraName <- function(x, ...) {
   .ui <- x[[1]]
-  paste0(.ui$modelName, ".contra")
+  paste0(rxUiGet.nonmemExportPath(x, ...), ".contra")
 }
 
 #' @export
 rxUiGet.nonmemCcontraName <- function(x, ...) {
   .ui <- x[[1]]
-  paste0(.ui$modelName, ".ccontra")
+  paste0(rxUiGet.nonmemExportPath(x, ...), ".ccontra")
 }
 
 #' @export
 rxUiGet.nonmemCsv <- function(x, ...) {
   .ui <- x[[1]]
-  paste0(.ui$modelName, ".csv")
+  paste0(rxUiGet.nonmemExportPath(x, ...), ".csv")
 }
 
 #' @export
 rxUiGet.nonmemNmctl <- function(x, ...) {
   .ui <- x[[1]]
-  paste0(.ui$modelName, rxode2::rxGetControl(.ui, "extension", ".nmctl"))
+  paste0(rxUiGet.nonmemExportPath(x, ...), rxode2::rxGetControl(.ui, "extension", ".nmctl"))
 }
 
 #' @export
 rxUiGet.nonmemQs <- function(x, ...) {
   .ui <- x[[1]]
-  paste0(.ui$modelName, ".qs")
+  paste0(rxUiGet.nonmemExportPath(x, ...), ".qs")
 }
