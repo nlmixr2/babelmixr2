5b1e3a75d8c060a01049802f0689375b0cf42249
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-06-10 14:18:02 -0500

    Add obji

--- R/nonmemRxUiGetTable.R
+++ R/nonmemRxUiGetTable.R
@@ -1,6 +1,6 @@
 #' @export
 rxUiGet.nonmemTable <- function(x, ...) {
-  paste(c("$TABLE ID ETAS(1:LAST) FIRSTONLY ONEHEADER NOPRINT\n",
+  paste(c("$TABLE ID ETAS(1:LAST) OBJI FIRSTONLY ONEHEADER NOPRINT\n",
           "    NOAPPEND FILE=",
           rxUiGet.nonmemEtaTableName(x, ...),"\n\n",
           "$TABLE ID TIME IPRED ONEHEADER NOPRINT\n",
