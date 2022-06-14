61b48800c341f9b0c7b25f48504de41a44e1cc65
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-06-14 08:43:16 -0500

    Align all the types

--- R/nonmemRxUiGetSub.R
+++ R/nonmemRxUiGetSub.R
@@ -3,7 +3,7 @@ rxUiGet.nonmemSub <- function(x, ...) {
   .ui <- x[[1]]
   .advan <- toupper(rxode2::rxGetControl(.ui, "advanOde", "advan13"))
   .ret <- paste0("$SUBROUTINES ", .advan, " TOL=", rxode2::rxGetControl(.ui, "tol", 6))
-  .c <- rxUiGet.nonmemContr(x, ...)
+  .c <- rxUiGet.nonmemContra(x, ...)
   if (!is.null(.c)) {
     .ret <- paste0(.ret, "\n  ",
                    "CONTR=",
