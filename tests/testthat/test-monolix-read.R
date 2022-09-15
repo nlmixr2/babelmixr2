b41021f69d737bec5c157befca431a93afd0a39e
Matthew Fidler <matthew.fidler@gmail.com>
2022-09-14 21:41:23 -0500

    Add missing modelName (for current nlmixr2)

--- tests/testthat/test-monolix-read.R
+++ tests/testthat/test-monolix-read.R
@@ -436,7 +436,8 @@ test_that("Monolix wbc", {
     
     unzip(.pathCharts)
     
-    f <- suppressWarnings(nlmixr2::nlmixr2(wbc, nlmixr2data::wbcSim, "monolix"))
+    f <- suppressWarnings(nlmixr2::nlmixr2(wbc, nlmixr2data::wbcSim, "monolix",
+                                           control=monolixControl(modelName="wbc")))
     expect_true(inherits(f, "nlmixr2FitData"))
     expect_true(inherits(f$env$parHist, "data.frame"))
     
