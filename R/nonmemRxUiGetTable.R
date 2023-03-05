e98f11d6922e21bd0dfe77bdec8b4dafb2aa8802
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-03-04 22:42:52 -0600

    Fix #73

--- R/nonmemRxUiGetTable.R
+++ R/nonmemRxUiGetTable.R
@@ -1,10 +1,10 @@
 #' @export
 rxUiGet.nonmemTable <- function(x, ...) {
   paste(c("$TABLE ID ETAS(1:LAST) OBJI FIRSTONLY ONEHEADER NOPRINT\n",
-          "    NOAPPEND FILE=",
+          "     FORMAT=s1PE17.9 NOAPPEND FILE=",
           rxUiGet.nonmemEtaTableName(x, ...),"\n\n",
           "$TABLE ID TIME IPRED PRED RXROW ONEHEADER NOPRINT\n",
-          "    NOAPPEND FILE=",
+          "    FORMAT=s1PE17.9 NOAPPEND FILE=",
           rxUiGet.nonmemSdTableName(x, ...),"\n"
           ), collapse="")
 }
