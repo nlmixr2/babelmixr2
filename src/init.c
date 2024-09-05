26605325c362ac8cca0d2dfeabf392962c4ffbb1
Matthew Fidler <matthew.fidler@gmail.com>
2024-09-05 17:14:53 -0500

    Export solve n2

--- src/init.c
+++ src/init.c
@@ -17,11 +17,13 @@ SEXP _babelmixr2_popedSetup(SEXP eSEXP, SEXP fullSEXP);
 SEXP _babelmixr2_popedSolveIdN(SEXP thetaSEXP, SEXP mtSEXP, SEXP idSEXP, SEXP totnSEXP);
 SEXP _babelmixr2_popedSolveIdME(SEXP thetaSEXP, SEXP umtSEXP, SEXP mtSEXP, SEXP msSEXP, SEXP nendSEXP, SEXP idSEXP, SEXP totnSEXP);
 SEXP _babelmixr2_popedSolveIdME2(SEXP thetaSEXP, SEXP umtSEXP, SEXP mtSEXP, SEXP msSEXP, SEXP nendSEXP, SEXP idSEXP, SEXP totnSEXP);
+SEXP _babelmixr2_popedSolveIdN2(SEXP thetaSEXP, SEXP mtSEXP, SEXP idSEXP, SEXP totnSEXP);
 
 static const R_CallMethodDef CallEntries[] = {
   {"_babelmixr2_popedFree", (DL_FUNC) &_babelmixr2_popedFree, 0},
   {"_babelmixr2_popedSetup", (DL_FUNC) &_babelmixr2_popedSetup, 2},
   {"_babelmixr2_popedSolveIdN", (DL_FUNC) &_babelmixr2_popedSolveIdN, 4},
+  {"_babelmixr2_popedSolveIdN2", (DL_FUNC) &_babelmixr2_popedSolveIdN, 4},
   {"_babelmixr2_popedSolveIdME", (DL_FUNC) &_babelmixr2_popedSolveIdME, 7},
   {"_babelmixr2_popedSolveIdME2", (DL_FUNC) &_babelmixr2_popedSolveIdME2, 7},
   {"_babelmixr2_iniRxodePtrs", (DL_FUNC) &_babelmixr2_iniRxodePtrs, 1},
