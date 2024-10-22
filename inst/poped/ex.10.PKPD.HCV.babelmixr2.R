47e1187b823afa289b6f942eff4f79be74fbcbc0
Theodoros Papathanasiou <theod.papath@gmail.com>
2024-10-22 16:25:19 +0200

    example 10 corrections added

--- inst/poped/ex.10.PKPD.HCV.babelmixr2.R
+++ inst/poped/ex.10.PKPD.HCV.babelmixr2.R
@@ -46,7 +46,7 @@ f <- function() {
             n <- exp(tn + eta.n)
             delta <- exp(tdelta + eta.delta)
             c <- exp(tc + eta.c)
-
+            
             # # Apply the conditional logic -
             # # This does not work right now - could be addressed in the future
             # if ( (time - (floor(time / TAU) * TAU)) < TINF) {
@@ -54,7 +54,7 @@ f <- function() {
             # } else {
             #       In <- 0  # No infusion
             # }
-
+            
             d/dt(depot)   = -KA*depot #  + In
             d/dt(central) =  KA*depot - KE*central
             d/dt(TC) = s - TC*(e*VP+d)    # target cells (TC)
@@ -62,7 +62,7 @@ f <- function() {
             d/dt(VP) = p*(1-(pow(central/VD,n)/(pow(central/VD,n)+pow(EC50,n))))*IC-c*VP # viral particles (VP)
             
             # Initial conditions
-            dur(depot) = TINF
+            dur(depot) = 1
             # depot(0) <- DOSE
             
             TC(0) =c*delta/(p*e)
@@ -83,24 +83,23 @@ f <- f()
 
 TAU = 7
 e1 <- et(c( 0,0.25,0.5,1,2,3,4,7,10,14,21,28)) %>%
-      add.dosing(dose=180, nbr.doses=4, dosing.interval=TAU) %>% 
+      add.dosing(dose=180, nbr.doses=4, dosing.interval=TAU) %>%
       as.data.frame() %>%
       dplyr::mutate(dvid=1)
 
 e2 <- et(c( 0,0.25,0.5,1,2,3,4,7,10,14,21,28)) %>%
-      add.dosing(dose=0, nbr.doses=4, dosing.interval=TAU) %>% 
+      add.dosing(dose=0, nbr.doses=4, dosing.interval=TAU) %>%
       as.data.frame() %>%
-      dplyr::mutate(dvid=2) %>% 
+      dplyr::mutate(dvid=2) %>%
       dplyr::filter(is.na(ii))
 
 e <- rbind(e1, e2)
 
 babel.db <- nlmixr2(f, e, "poped",
                     popedControl(
-                          groupsize=30,
-                          a=list(c(TINF=1))))
-                          # a=list(c(DOSE=180,TINF=1,TAU=7))
-                    # ))
+                          groupsize=30
+                          # ,a=list(c(DOSE=180,TINF=1,TAU=7))
+                    ))
 
 ##  create plot of model without variability 
 plot_model_prediction(babel.db, facet_scales = "free")
@@ -111,13 +110,13 @@ shrinkage(babel.db)
 #' The reduced FIM 
 #' ####################################
  # computation time
-tic(); FIM_compiled <- evaluate.fim(babel.db); toc()
+tic(); FIM_babel <- evaluate.fim(babel.db); toc()
 
 #' design evaluation
-crit <- det(FIM_compiled)^(1/length(get_unfixed_params(babel.db)[["all"]]))
+crit <- det(FIM_babel)^(1/length(get_unfixed_params(babel.db)[["all"]]))
 crit
-rse <- get_rse(FIM_compiled,babel.db) # this is for the log of the fixed effect parameters
-rse_norm <- sqrt(diag(inv(FIM_compiled)))*100 # this is approximately the RSE for the normal scale of the fixed effects 
+rse <- get_rse(FIM_babel,babel.db) # this is for the log of the fixed effect parameters
+rse_norm <- sqrt(diag(inv(FIM_babel)))*100 # this is approximately the RSE for the normal scale of the fixed effects 
 rse[1:7] <- rse_norm[1:7] # replace the log scale for the normal scale RSE values
 rse
 
@@ -135,13 +134,13 @@ rse_reference_reduced <- c(12.1,10.5,10.0,15.8,10.4,9.4,11.0,40.0,30.8,28.8,60.4
 #' #####################################
 #' The full FIM. 
 #' #####################################
-FIM_compiled_full <- evaluate.fim(poped_db_compiled,fim.calc.type = 0) 
+FIM_babel_full <- evaluate.fim(babel.db,fim.calc.type = 0) 
 
 #' design evaluation
-crit_full <- det(FIM_compiled_full)^(1/length(get_unfixed_params(poped_db_compiled)[["all"]]))
+crit_full <- det(FIM_babel_full)^(1/length(get_unfixed_params(babel.db)[["all"]]))
 crit_full
-rse_full <- get_rse(FIM_compiled_full,poped_db_compiled) # this is for the log of the fixed effect parameters
-rse_norm_full <- sqrt(diag(inv(FIM_compiled_full)))*100 # this is approximately the RSE for the normal scale of the fixed effects 
+rse_full <- get_rse(FIM_babel_full,babel.db) # this is for the log of the fixed effect parameters
+rse_norm_full <- sqrt(diag(inv(FIM_babel_full)))*100 # this is approximately the RSE for the normal scale of the fixed effects 
 rse_full[1:7] <- rse_norm_full[1:7] # replace the log scale for the normal scale RSE values
 rse_full
 
