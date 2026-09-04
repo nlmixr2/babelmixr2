dba6ae02e4511e7ccf8b1e8a6a98112aa5c348a0
Justin Wilkins <justin.wilkins@occams.com>
2026-09-04 18:18:12 +0200

    test(pharmml): schema-validate the math walker output
    Confirms ct:SymbolIdType (xs:NCName) accepts dotted names like eta.cl, so no
    name-mangling layer is needed when the section writers land.

--- tests/testthat/test-pharmml-math.R
+++ tests/testthat/test-pharmml-math.R
@@ -148,3 +148,46 @@ test_that(".rxToPharmml handles nested ifelse()", {
   .x <- .rxToPharmml(quote(ifelse(a > 1, x, ifelse(b > 2, y, z))))
   expect_equal(lengths(regmatches(.x, gregexpr("<math:Piecewise>", .x))), 2L)
 })
+
+# Wrap a math subtree in the smallest document the schema will accept, so the
+# walker's output can be validated in isolation.
+.pharmmlWrapMath <- function(x) {
+  paste0(
+    '<?xml version="1.0" encoding="UTF-8"?>\n',
+    '<PharmML xmlns="http://www.pharmml.org/pharmml/0.9/PharmML"\n',
+    '    xmlns:ct="http://www.pharmml.org/pharmml/0.9/CommonTypes"\n',
+    '    xmlns:math="http://www.pharmml.org/pharmml/0.9/Maths"\n',
+    '    xmlns:mdef="http://www.pharmml.org/pharmml/0.9/ModelDefinition"\n',
+    '    writtenVersion="0.9" id="i1">\n',
+    '  <ct:Name>math walker fixture</ct:Name>\n',
+    '  <IndependentVariable symbId="t"/>\n',
+    '  <mdef:ModelDefinition>\n',
+    '    <mdef:StructuralModel blkId="sm1">\n',
+    '      <ct:Variable symbId="y" symbolType="real">\n',
+    '        <ct:Assign>\n', x, '\n</ct:Assign>\n',
+    '      </ct:Variable>\n',
+    '    </mdef:StructuralModel>\n',
+    '  </mdef:ModelDefinition>\n',
+    '</PharmML>\n')
+}
+
+test_that("the math walker emits schema-valid PharmML", {
+  .exprs <- list(
+    quote(a + b),
+    quote(-a),
+    quote(ka * depot - cl / v * central),
+    quote(exp(tcl + eta.cl)),
+    quote(logit(a)),
+    quote(expit(a)),
+    quote(log1p(a)),
+    quote(atan2(a, b)),
+    quote(ifelse(wt > 70, a, b)),
+    quote(if (wt > 70) a else b),
+    quote(ifelse(a > 1, x, ifelse(b > 2, y, z)))
+  )
+  for (.e in .exprs) {
+    .doc <- .pharmmlWrapMath(.rxToPharmml(.e))
+    expect_true(pharmmlValidate(.doc),
+                info = paste("failed for:", deparse1(.e)))
+  }
+})
